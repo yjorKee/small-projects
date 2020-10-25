2f4cd304c8616215c35e236652dc34da4a24aea0
Shantanu Misra <shantanumisra00@mail.com>
2020-10-25 22:03:47 +0530

    Update binary_to_roman_numbers.py

--- binary_to_roman_numbers.py
+++ binary_to_roman_numbers.py
@@ -4,15 +4,15 @@ roman_number = ''
 
 numbers_with_letter_representation = [1000, 500, 100, 50, 10, 5, 1]
 frequencies = [0, 0, 0, 0, 0, 0, 0]
-frequency_index = 6
+frequency_index = 0
 
 for number in numbers_with_letter_representation:
     number_of_leters = decimal_number // number
     decimal_number -= number * (number_of_leters)
     frequencies[frequency_index] = number_of_leters
-    frequency_index -= 1
+    frequency_index += 1
 
-roman_number += 'M'*frequencies[6] + 'D'*frequencies[5] + 'C'*frequencies[4] + 'L'*frequencies[3] + 'X'*frequencies[2] + 'V'*frequencies[1] + 'I'*frequencies[0]
+roman_number += 'M'*frequencies[0] + 'D'*frequencies[1] + 'C'*frequencies[2] + 'L'*frequencies[3] + 'X'*frequencies[4] + 'V'*frequencies[5] + 'I'*frequencies[6]
 
 roman_number = roman_number.replace('CCCC', 'CD')
 roman_number = roman_number.replace('XXXX', 'XL')
