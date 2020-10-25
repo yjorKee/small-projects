4c209aff62e9a06a5a37223fc671e5c1f6831da4
Shantanu Misra <shantanumisra00@mail.com>
2020-10-25 22:01:06 +0530

    Update binary_to_roman_numbers.py

--- binary_to_roman_numbers.py
+++ binary_to_roman_numbers.py
@@ -1,32 +1,22 @@
-binary_number = int(input('Enter a binary number: '))
-non_altered__binary_number = binary_number
+decimal_number = int(input('Enter a decimal number: '))
+non_altered__decimal_number = decimal_number
 roman_number = ''
 
-numbers_with_letter_representation = [1000, 500, 100, 50, 10]
-frequencies = [0, 0, 0, 0, 0]
-frequency_index = 4
+numbers_with_letter_representation = [1000, 500, 100, 50, 10, 5, 1]
+frequencies = [0, 0, 0, 0, 0, 0, 0]
+frequency_index = 6
 
 for number in numbers_with_letter_representation:
-    number_of_leters = binary_number // number
-    binary_number -= number * (number_of_leters)
+    number_of_leters = decimal_number // number
+    decimal_number -= number * (number_of_leters)
     frequencies[frequency_index] = number_of_leters
     frequency_index -= 1
 
-roman_number += 'M'*frequencies[4] + 'D'*frequencies[3] + 'C'*frequencies[2] + 'L'*frequencies[1] + 'X'*frequencies[0]
+roman_number += 'M'*frequencies[6] + 'D'*frequencies[5] + 'C'*frequencies[4] + 'L'*frequencies[3] + 'X'*frequencies[2] + 'V'*frequencies[1] + 'I'*frequencies[0]
 
-if (binary_number == 1) or (binary_number == 2) or (binary_number == 3):
-    roman_number += 'I'*binary_number
-elif binary_number == 4:
-    roman_number += 'IV'
-elif binary_number == 5:
-    roman_number += 'V'
-elif binary_number == 6:
-    roman_number += 'VI'
-elif binary_number == 7:
-    roman_number += 'VII'
-elif binary_number == 8:
-    roman_number += 'VIII'
-elif binary_number == 9:
-    roman_number += 'IX'
+roman_number = roman_number.replace('CCCC', 'CD')
+roman_number = roman_number.replace('XXXX', 'XL')
+roman_number = roman_number.replace('IIII', 'IV')
+roman_number = roman_number.replace('VIV', 'IX')
 
-print(f'{non_altered__binary_number} in roman numbers is: {roman_number}')
+print(f'{non_altered__decimal_number} in roman numbers is: {roman_number}')
